ff400b3b74172f56e61436866b3cb6a06ae60068
Joshi <Himanshu.Joshi@iso-gruppe.com>
2025-07-19 15:50:11 +0200

    Added Remove Sub Folders From The File System

--- 1/1000s/12s/1233/RemoveSubFoldersFromTheFileSystem.cpp
+++ 1/1000s/12s/1233/RemoveSubFoldersFromTheFileSystem.cpp
@@ -50,3 +50,74 @@ public:
         return res;
     }
 };
+
+// ####### ------- #######
+
+class Solution2 {
+public:
+    vector<string> removeSubfolders(vector<string>& folder) {
+        sort(folder.begin(), folder.end());
+        vector<string>res;
+        for(auto &paths: folder){
+            if(res.size()>=1){
+                string curbck=res.back();
+                string tochk=paths.substr(0, curbck.size());
+                if(tochk!=curbck||paths[curbck.size()]!='/'){
+                    res.push_back(paths);
+                }
+            } else{
+                res.push_back(paths);
+            }
+        }
+        return res;
+    }
+};
+
+// ####### ------- #######
+
+class CustomComparator{
+    public:
+    bool operator()(const string &A, const string &B){
+        return A.length()<B.length();
+    }
+};
+
+class Solution_TLE {
+public:
+
+    bool isSub(string par, string tochk){
+        int sz=tochk.size(), idx=1;
+        string tofrm="";
+        tofrm+=tochk[0];
+        for(; idx<=sz-1; ){
+            tofrm+=tochk[idx];
+            if(tofrm==par) return true;
+            ++idx;
+            while(true){
+                if(tochk[idx]=='/') break;
+                tofrm+=tochk[idx];
+                idx++;
+                if(idx>=sz) break;
+            }
+            if(tofrm==par) return true;
+        }
+        return false;
+    }
+
+    vector<string> removeSubfolders(vector<string>& folder) {
+        sort(folder.begin(), folder.end(), CustomComparator());
+        for(int i=0; i<folder.size(); ++i){
+            string curr=folder[i];
+            for(int j=0; j<folder.size(); ++j){
+                if(i!=j){
+                    string next=folder[j];
+                    if(isSub(curr, next)){
+                        folder[j]="#";
+                    }
+                }
+            }
+            folder.erase(remove(folder.begin(), folder.end(), "#"), folder.end());
+        }
+        return folder;
+    }
+};
